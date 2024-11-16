037663874bcf1db3e0a08be3f28d09256181b1e5
Mahdi Asghari <mahdi.asghari.saedi@gmail.com>
2024-11-17 00:29:10 +0330

    First Commit

---
+++ src/components/Result.jsx
@@ -0,0 +1,15 @@
+export default function Result({ duration }) {
+ 
+  return (
+    <table>
+      <thead id="result thead">
+        <tr>
+          <th>Year</th>
+          <th>Year</th>
+          <th>Year</th>
+          <th>Year</th>
+        </tr>
+      </thead>
+    </table>
+  );
+}
